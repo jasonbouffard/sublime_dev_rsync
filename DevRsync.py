c24c602e9e76435d4d097f4d883a649394d49dc1
Jason Bouffard <jasonbouffard@gmail.com>
2012-02-12 14:48:43 -0500

    now using a subprocess

--- DevRsync.py
+++ DevRsync.py
@@ -1,26 +1,35 @@
-import sublime, sublime_plugin, json, os
+import sublime, sublime_plugin
+import json, os, subprocess, functools
 
 class DevRsyncCommand(sublime_plugin.EventListener):
     def on_post_save(self, view):
         cmd = "rsync"
+
+        # Check if we're in a proper ST2 project
+        if not view.window().folders():
+            return
+
         projectFolder = view.window().folders()[0]
         projectFile   = projectFolder + '/dev_rsync.json'
-        
+
         # Skip syncing if there is no dev_rsync.json file
         if os.path.exists(projectFile):
             json_data = open(projectFile)
-            data = json.load(json_data)
+            data      = json.load(json_data)
             json_data.close
-            
+
             for option in data['option']:
                 cmd += ' --' + option
-            
+
             for exclude in data['exclude']:
                 cmd += ' --exclude="' + exclude + '"'
 
-            cmd += ' ' + projectFolder + '/' + ' ' + data['host'] + ':' + data['target_dir'] + '/' + ' &'
-
-            print cmd
+            cmd += ' ' + projectFolder + '/' + ' ' + data['host'] + ':' + data['target_dir'] + '/'
 
             # Execute the rsync command
-            os.system(cmd)
+            p = subprocess.call(cmd, shell = True)
+
+            sublime.set_timeout(functools.partial(self.updateStatus, view, 'DevSync Done!'), 100)
+
+    def updateStatus(self, view, text):
+        sublime.status_message(text);
