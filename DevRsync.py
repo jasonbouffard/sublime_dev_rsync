0b6c21fca50c7881c2b9083dad0dfba156b2ee62
Jason Bouffard <jasonbouffard@gmail.com>
2012-02-12 13:28:18 -0500

    white space to spaces and removed debug

--- DevRsync.py
+++ DevRsync.py
@@ -1,25 +1,24 @@
 import sublime, sublime_plugin, json, os
 
 class DevRsyncCommand(sublime_plugin.EventListener):
-	def on_post_save(self, view):
-		cmd = "rsync"
-		projectFolder = view.window().folders()[0]
-		projectFile = projectFolder + '/dev_rsync.json'
-		
-		if os.path.exists(projectFile):
-			json_data = open(projectFile)
-			data = json.load(json_data)
-			json_data.close
-			sublime.error_message('hi')
-			return
-			for option in data['option']:
-				cmd += ' --' + option
-			
-			for exclude in data['exclude']:
-				cmd += ' --exclude="' + exclude + '"'
-		
-			cmd += ' ' + projectFolder + '/' + ' ' + data['host'] + ':' + data['target_dir'] + '/' + ' &'
+    def on_post_save(self, view):
+        cmd = "rsync"
+        projectFolder = view.window().folders()[0]
+        projectFile = projectFolder + '/dev_rsync.json'
+        
+        if os.path.exists(projectFile):
+            json_data = open(projectFile)
+            data = json.load(json_data)
+            json_data.close
+            
+            for option in data['option']:
+                cmd += ' --' + option
+            
+            for exclude in data['exclude']:
+                cmd += ' --exclude="' + exclude + '"'
+        
+            cmd += ' ' + projectFolder + '/' + ' ' + data['host'] + ':' + data['target_dir'] + '/' + ' &'
 
-			print cmd
+            print cmd
 
-			os.system(cmd)
+            os.system(cmd)
